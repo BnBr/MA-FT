5c54909b8bbea56ac1cbdcd1f3939fec7c84c041
Jan <seewaldjan@users.noreply.github.com>
2017-08-14 18:00:46 +0200

    some navbar stuff with bulma

--- src/components/App-Nav/App-Nav.js
+++ src/components/App-Nav/App-Nav.js
@@ -1,8 +1,21 @@
 export default {
     name: 'app-nav',
-    data() {
+    data: function() {
         return {
-            greeting: 'Hallo Ich!'
+            activeTab: '_'
         }
+    },
+        methods: {
+            isActive: function(name){
+                if(this.activeTab == name) {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+            
+            setActive: function(name) {
+                this.activeTab = name;
+            }
     }
 }
